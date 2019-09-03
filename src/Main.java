aa09081c277018b1a5470e33970f39b7c3b06cc7
Danil <lilkit1996@gmail.com>
2019-09-03 18:10:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Lesson7 lesson7 = new Lesson7();
+        lesson7.pullingUp(lesson7.logic());
+        lesson7.pullingUpForTwenty();
+    }
+}
