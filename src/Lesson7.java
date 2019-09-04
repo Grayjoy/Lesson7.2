038e09fdb987c9c9a3fa979e67a52a2c9cb0e92b
Danil <lilkit1996@gmail.com>
2019-09-04 14:09:07 +0300

    1.02

--- src/Lesson7.java
+++ src/Lesson7.java
@@ -6,40 +6,41 @@ public class Lesson7 {
     private int onePullingUp = 5;
     private int rest = 60;
 
-    private int scr;
-    private int valueInWork;
 
-    public int input() {
+
+    public  int input() {
 
         System.out.println("Введите количество подходов");
-        scr = scanner.nextInt();
-
+        int scr = scanner.nextInt();
         return scr;
+
+
     }
 
     public int pullingUp(int scr) {
         int value = scr * (scr + 1) / 2;
         System.out.println("Кол-во подтягиваний: " + value);
 
-        valueInWork = value * onePullingUp;
+        int valueInWork = value * onePullingUp;
         int valueInRest = scr * rest;
         int valueAll = valueInWork + valueInRest - 60;
 
-        System.out.println("Количество времени выполнения: " + valueAll / 60 + " минуты " + valueAll % 60 + " секунд");
+        System.out.println("Количество времени выполнения: " + valueAll / 60 + ":" + valueAll % 60 );
         return valueInWork;
+
     }
 
-    public void pullingUpForTwenty() {
+    public void pullingUpForTwenty(int valueInWork,int scr) {
         double restForTwenty = 60;
         for (int i = 1; i < scr - 1; i++) {
-            double scr1 = restForTwenty * 0.2;
+            double scrForTwenty = restForTwenty * 0.2;
             restForTwenty += restForTwenty;
-            restForTwenty = scr1 + restForTwenty;
+            restForTwenty = scrForTwenty + restForTwenty;
 
         }
         if (scr > 1) {
             int valueAll2 = (int) (restForTwenty + valueInWork);
-            System.out.println("Количество времени выполнения с учетом добавления 20%: " + valueAll2 / 60 + " минуты " + valueAll2 % 60 + " секунд");
+            System.out.println("Количество времени выполнения с учетом добавления 20%: " + valueAll2 / 60 + ":" + valueAll2 % 60 );
 
         } else {
             System.out.println("Количество времени выполнения: " + 0);
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Lesson7 lesson7 = new Lesson7();
-        lesson7.pullingUp(lesson7.input());
-        lesson7.pullingUpForTwenty();
+        int inp = lesson7.input();
+        lesson7.pullingUpForTwenty(lesson7.pullingUp(inp), inp);
     }
 }
